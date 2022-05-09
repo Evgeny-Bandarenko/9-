e7f4af612f361a957f3612f5298c0ba21e4b7684
Evgeny <104760685+Evgeny-Bandarenko@users.noreply.github.com>
2022-05-09 22:24:06 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,24 @@
+UP_PAYMENT = 800
+MAX_PAYMENT = 4500
+TO_SAVE = 0.7
+payment = 300
+current_age = 27
+total_money = 0
+
+CAR_PRICE = 300000
+is_car_byeid = False
+House_price = 600000
+is_House_byelid = False
+while current_age <= 60:
+    total_money += 12 * payment * TO_SAVE
+    if payment < MAX_PAYMENT:
+        payment += UP_PAYMENT
+    if total_money >= CAR_PRICE and not is_car_byeid:
+        print('Я могу позволите себе ламборгини')
+        is_car_byeid = True
+    if total_money >= House_price and not is_House_byelid:
+        print('Я могу позволите себе дом')
+        is_House_byelid = True
+
+    print('Нам сейчас: ', current_age, 'У нас сейчас: ', total_money)
+    current_age += 1
---
+++ word_1.py
@@ -0,0 +1,28 @@
+# 2.1
+#
+# line = "rythm rough rush shake than"
+# print(line.replace('a', ' '), line.count("a"))
+
+line = "rythm rough rush shake than"
+print(line.replace('a', ' '))
+print(line.count("a"))
+
+# 2.2
+# line = "rythm (rough rush shake) than"
+# print(line.find("("), line.find(")"))
+# print(line[:5], line[24:])
+
+# # 2.3
+# line = "ythm rough rush shake than"
+# print(line.count("t"))
+
+# 2.4
+# line = "rythm rough rush shake than"
+# print(line.find("h"), line.rfind("h"))
+# print(line[-4:-24:-1])
+
+# # 2.5
+# line_1 = "rythm rough rush shake than"
+# line = "rythm rough rush shake than"
+# line = line.replace("h", "H")
+# print(line_1 [:10] + line[10:24] + line_1[24:])
